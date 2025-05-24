a8551e87fc5ce0790770274a4ec3cd25075fae3d
T.Wada <28886385+TakeshiWada1980@users.noreply.github.com>
2025-05-24 10:19:00 +0900

    update CartItem.ts and route.ts

--- src/app/_types/CartItem.ts
+++ src/app/_types/CartItem.ts
@@ -2,7 +2,7 @@ import { z } from "zod"; // zodライブラリのインポート
 
 // バリデーションスキーマ（データが満たすべき制約条件や構造を定義）
 export const cartItemSchema = z.object({
-  productId: z.string().min(1), // 1文字以上の「文字列」
+  productId: z.string().min(1).max(10), // 1文字以上10文字以下の「文字列」
   quantity: z.number().int().min(0), // 0以上の「整数値」
 });
 
